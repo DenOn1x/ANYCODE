7b2cfd0192ec055e4ea85bce05a2b6eb6d05bb7f
greck <d.popov@yandex.by>
2024-07-05 17:19:12 +0300

    [IMP] Anycode: nginx-config Added config file for images

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    }
-})
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    // server: {
+    //     port: 6666,
+    // },
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    const extType = assetInfo.name.split('.').pop();
+                    if (/png|jpe?g|gif|svg/i.test(extType)) {
+                        return 'assets/images/[name].[hash].[ext]';
+                    }
+                    return 'assets/[name].[hash].[ext]';
+                },
+            },
+        },
+    },
+});
