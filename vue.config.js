209ebedaae34c4c38675e922861a362c9954c847
greck <d.popov@yandex.by>
2024-07-05 15:56:50 +0300

    [IMP] Anycode: nginx-config Added config file for images

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 module.export = {
     devServer: {
         port: 6666
-    }
+    },
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[hash].[ext]',
+                                outputPath: 'assets/images',
+                                publicPath: '/assets/images',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+    },
 };
